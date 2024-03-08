ca3ff08815a117f2a7815723e66ef65ed19862d3
Eric Lee <ericleecodes@gmail.com>
2024-03-07 20:58:36 -0800

    Follow submission AI's suggestions and change accordingly

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -113,12 +113,13 @@ const getAllProperties = function(options, limit = 10) {
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
   `;
 
 
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += `AND city LIKE $${queryParams.length} `;
   }
 
   if (options.owner_id) {
@@ -127,20 +128,16 @@ const getAllProperties = function(options, limit = 10) {
   }
 
 
-  if (options.minimum_price_per_night) {
-    queryParams.push(options.minimum_price_per_night);
-    queryString += `AND cost_per_night > $${queryParams.length}`;
-  } else if (options.maximum_price_per_night) {
-    queryParams.push(options.maximum_price_per_night);
-    queryString += `AND cost_per_night > $${queryParams.length}`;
-  } else if (options.minimum_price_per_night && options.maximum_price_per_night) {
-    queryParams.push(options.minimum_price_per_night);
-    queryString += `AND cost_per_night > $${queryParams.length} AND cost_per_night < $${queryParams.length} `;
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `AND (cost_per_night >= $${queryParams.length - 1
+      } AND cost_per_night <= $${queryParams.length})\n`;
   }
 
   if (options.minimum_rating) {
     queryParams.push(options.minimum_rating);
-    queryString += `AND average_rating >= $${queryParams.length}`;
+    queryString += `HAVING avg(rating) >= $${queryParams.length}\n`;
   }
 
 
