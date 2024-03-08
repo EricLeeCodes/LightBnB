913f301968db8b53890ccd30a76d6e5c93bf9ad8
Eric Lee <ericleecodes@gmail.com>
2024-03-07 21:39:26 -0800

    Add parentheses in addProperty

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -169,7 +169,7 @@ const getAllProperties = function(options, limit = 10) {
 
 const addProperty = (property) => {
   return pool
-    .query(`INSERT INTO properties owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms
+    .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
     VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
     RETURNING *;
     `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
